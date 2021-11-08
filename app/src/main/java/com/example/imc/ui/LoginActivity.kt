2fb841f2ba85afc86fa76f2df2561aa304421902
ana bea <anabeatrizpbrito02@gmail.com>
2021-11-08 11:19:35 -0300

    salvar

---
+++ app/src/main/java/com/example/imc/ui/LoginActivity.kt
@@ -0,0 +1,24 @@
+package com.example.imc.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.example.imc.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        supportActionBar!!.hide()
+
+        val tvNovaConta = findViewById<TextView>(R.id.tv_nova_conta)
+
+        tvNovaConta.setOnClickListener {
+            val novaConta = Intent(this, NovoUsuarioActivity::class.java)
+            startActivity(novaConta)
+
+        }
+    }
+}
